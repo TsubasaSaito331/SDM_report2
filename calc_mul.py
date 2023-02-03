3fc2e13da1502cc630048eab20bd2403cfb0d98b
TsubasaSaito331 <124391530+TsubasaSaito331@users.noreply.github.com>
2023-02-04 03:14:42 +0900

    Update calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -6,7 +6,7 @@ def calc(A,B):
         ai=str(A)
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        if p.match(ai) and p.match(bi):
                 a=float(ai)
                 b=float(bi)
                 if 0<a and a<b and b<1000:
